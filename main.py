ddffd7d96c9332e22fc9557adfa2454038958ab1
shashank <iamshashankpeddi@gmail.com>
2021-04-17 10:48:48 +0530

    changed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -8,9 +9,17 @@ def hello():
     return render_template('page.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def shashank():
+    if request.method == "POST":
+        details = request.form
+        name = details['name']
+        email = details['email']
+        password = details['password']
+
+        return 'success'
     return render_template('login.html')
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
